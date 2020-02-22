e1dde8b4a061d3d72d0871d9a3e183f73ebcf9a8
WagnerReis <wagnerreis1997@gmail.com>
2020-02-22 11:38:02 -0300

    Iniciando o Projeto

---
+++ src/base.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwjEW9biBLx7emq_fZzJwyHG0E5M0y2pM",
+    authDomain: "mercadodev-509f1.firebaseapp.com",
+    databaseURL: "https://mercadodev-509f1.firebaseio.com",
+    projectId: "mercadodev-509f1",
+    storageBucket: "mercadodev-509f1.appspot.com",
+    messagingSenderId: "987329108964",
+    appId: "1:987329108964:web:417ac257a306c9341785f1"
+} 
+
+const Rebase = require('re-base')
+const firebase = require('firebase/app')
+require('firebase/database')
+
+const app = firebase.initializeApp(firebaseConfig)
+const base = Rebase.createClass(app.database())
+
+export default base
